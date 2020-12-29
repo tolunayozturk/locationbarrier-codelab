7f8d47bd184fe1d4eab53769ff12938f396a46c0
Tolunay Ozturk <tolunay.ozturk@huawei.com>
2020-12-29 18:03:11 +0300

    Initial commit

---
+++ app/src/main/java/com/tolunayozturk/barrierdemo/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.tolunayozturk.barrierdemo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.analytics.HiAnalytics;
+import com.huawei.hms.analytics.HiAnalyticsInstance;
+import com.huawei.hms.analytics.HiAnalyticsTools;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+import com.huawei.hms.support.hwid.ui.HuaweiIdAuthButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int SIGN_IN_REQUEST_CODE = 8888;
+    private static final int PERMISSION_REQUEST_CODE = 820;
+
+    private AccountAuthService mAuthService;
+    private AccountAuthParams mAuthParams;
+    HiAnalyticsInstance mHiAnalytics;
+
+    HuaweiIdAuthButton huaweiIdAuthButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        huaweiIdAuthButton = findViewById(R.id.btn_login);
+
+        HiAnalyticsTools.enableLog();
+        mHiAnalytics = HiAnalytics.getInstance(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_CODE);
+        }
+
+        mAuthParams = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setId().setUid().setProfile().setAuthorizationCode().createParams();
+        mAuthService = AccountAuthManager.getService(this, mAuthParams);
+
+        huaweiIdAuthButton.setOnClickListener(view -> signIn());
+    }
+
+    private void signIn() {
+        startActivityForResult(mAuthService.getSignInIntent(), SIGN_IN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            authAccountTask.addOnSuccessListener(authAccount -> {
+                Log.i(TAG, "authAccountTask: " + authAccount.getUnionId());
+                mHiAnalytics.setUserId(authAccount.getUnionId());
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }).addOnFailureListener(e -> Log.e(TAG, "authAccountTask: " + e.getMessage(), e));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            boolean isPermissionGranted = true;
+            for (int result : grantResults) {
+                if (result == PackageManager.PERMISSION_DENIED) {
+                    isPermissionGranted = false;
+                }
+            }
+
+            if (!isPermissionGranted) {
+                Toast.makeText(this, "Permission(s) denied! " +
+                        "You must give necessary permissions for this demo app to run properly.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission(s) granted! ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/tolunayozturk/barrierdemo/MainActivity.java
@@ -0,0 +1,173 @@
+package com.tolunayozturk.barrierdemo;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.huawei.hms.analytics.HiAnalytics;
+import com.huawei.hms.analytics.HiAnalyticsInstance;
+import com.huawei.hms.kit.awareness.Awareness;
+import com.huawei.hms.kit.awareness.barrier.AwarenessBarrier;
+import com.huawei.hms.kit.awareness.barrier.BarrierQueryRequest;
+import com.huawei.hms.kit.awareness.barrier.BarrierStatus;
+import com.huawei.hms.kit.awareness.barrier.BarrierUpdateRequest;
+import com.huawei.hms.kit.awareness.barrier.LocationBarrier;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String ENTER_BARRIER_LABEL = "ENTER_BARRIER_LABEL";
+    private static final String EXIT_BARRIER_LABEL = "EXIT_BARRIER_LABEL";
+
+    private PendingIntent mPendingIntent;
+    private LocationBarrierReceiver mBarrierReceiver;
+    HiAnalyticsInstance mHiAnalytics;
+
+    double latitude = 41.02456;
+    double longitude = 28.85843;
+    double radius = 250;
+
+    Chronometer mChronometer;
+    TextView tv_log;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_log = findViewById(R.id.tv_log);
+        mChronometer = findViewById(R.id.chronometer);
+
+        mHiAnalytics = HiAnalytics.getInstance(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            printLog("Permission(s) denied!");
+            return;
+        }
+
+        final String BARRIER_RECEIVER_ACTION = getApplication().getPackageName() + "LOCATION_BARRIER_RECEIVER_ACTION";
+        Intent intent = new Intent(BARRIER_RECEIVER_ACTION);
+        mPendingIntent = PendingIntent.getBroadcast(this, 1, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBarrierReceiver = new LocationBarrierReceiver();
+        registerReceiver(mBarrierReceiver, new IntentFilter(BARRIER_RECEIVER_ACTION));
+
+        Awareness.getBarrierClient(this).queryBarriers(BarrierQueryRequest.all())
+                .addOnSuccessListener(barrierQueryResponse -> {
+                    for (String l : barrierQueryResponse.getBarrierStatusMap().getBarrierLabels()) {
+                        removeBarrier(l);
+                    }
+
+                    AwarenessBarrier enterBarrier = LocationBarrier.enter(
+                            latitude, longitude, radius);
+                    AwarenessBarrier exitBarrier = LocationBarrier.exit(
+                            latitude, longitude, radius);
+
+                    addBarrier(this, ENTER_BARRIER_LABEL, enterBarrier, mPendingIntent);
+                    addBarrier(this, EXIT_BARRIER_LABEL, exitBarrier, mPendingIntent);
+                }).addOnFailureListener(e -> Log.e(TAG, e.getMessage(), e));
+
+        // Test event
+//        Bundle bundle = new Bundle();
+//        bundle.putString("test_key", "test_value");
+//        mHiAnalytics.onEvent("TEST_EVENT", bundle);
+    }
+
+    private void addBarrier(Context context, final String label,
+                            AwarenessBarrier barrier, PendingIntent pendingIntent) {
+        BarrierUpdateRequest.Builder builder = new BarrierUpdateRequest.Builder();
+        BarrierUpdateRequest request = builder.addBarrier(label, barrier, pendingIntent).build();
+        Awareness.getBarrierClient(context).updateBarriers(request)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(context, "add " + label + " success",
+                            Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "add " + label + " success");
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(context, "add " + label + " failed",
+                            Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "add " + label + " failed", e);
+                });
+    }
+
+    private void removeBarrier(String label) {
+        BarrierUpdateRequest.Builder builder = new BarrierUpdateRequest.Builder();
+        builder.deleteBarrier(label);
+
+        Awareness.getBarrierClient(this)
+                .updateBarriers(builder.build()).addOnSuccessListener(aVoid -> {
+            Log.i(TAG, "removeBarrier: success");
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "removeBarrier: " + e.getMessage(), e);
+        });
+    }
+
+    private void printLog(String msg) {
+        DateFormat formatter = SimpleDateFormat.getDateTimeInstance();
+        String time = formatter.format(new Date(System.currentTimeMillis()));
+        tv_log.append("[" + time + "] " + msg + "\n");
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBarrierReceiver != null) {
+            unregisterReceiver(mBarrierReceiver);
+        }
+        super.onDestroy();
+    }
+
+    final class LocationBarrierReceiver extends BroadcastReceiver {
+        private final String TAG = LocationBarrierReceiver.class.getSimpleName();
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BarrierStatus barrierStatus = BarrierStatus.extract(intent);
+            String label = barrierStatus.getBarrierLabel();
+            switch (barrierStatus.getPresentStatus()) {
+                case BarrierStatus.TRUE:
+                    Log.i(TAG, "[" + label + "]" + " BarrierStatus: TRUE");
+                    printLog("[" + label + "]" + " BarrierStatus: TRUE");
+                    if (label.equals("ENTER_BARRIER_LABEL")) {
+                        mChronometer.setBase(SystemClock.elapsedRealtime());
+                        mChronometer.start();
+                    } else if (label.equals("EXIT_BARRIER_LABEL")) {
+                        mChronometer.stop();
+                        double elapsedMillis = SystemClock.elapsedRealtime() -
+                                mChronometer.getBase();
+                        printLog("Length of stay: " + elapsedMillis / 1000 + " seconds");
+                        Log.e(TAG, "Length of stay: " + elapsedMillis);
+
+                        Bundle bundle = new Bundle();
+                        bundle.putString("length_of_stay", String.valueOf(elapsedMillis / 1000));
+                        mHiAnalytics.onEvent("LENGTH_OF_STAY", bundle);
+                    }
+                    break;
+                case BarrierStatus.FALSE:
+                    Log.i(TAG, "[" + label + "]" + " BarrierStatus: FALSE");
+                    printLog("[" + label + "]" + " BarrierStatus: FALSE");
+                    break;
+                case BarrierStatus.UNKNOWN:
+                    Log.i(TAG, "[" + label + "]" + " BarrierStatus: UNKNOWN");
+                    printLog("[" + label + "]" + " BarrierStatus: UNKNOWN");
+                    break;
+            }
+        }
+    }
+}
